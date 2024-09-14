b201d519f2535b92f50f1066bb7dddb4441676e4
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-14 20:51:07 +0530

    generate 2 users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
+        User::factory()->create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }

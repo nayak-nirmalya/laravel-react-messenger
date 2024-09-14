89f0b591bc5d38c6095776c4a15215132f9f81f0
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-14 20:56:06 +0530

    generate User & Group and assign them

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,5 +25,16 @@ class DatabaseSeeder extends Seeder
             'email' => 'jane@example.com',
             'password' => bcrypt('password'),
         ]);
+
+        User::factory(10)->create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
     }
 }

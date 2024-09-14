795a1bfc824a6ca1b8b6ccc23d60c0c111c20910
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-14 21:08:04 +0530

    insert Message, Conversation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Conversation;
 use App\Models\Group;
+use App\Models\Message;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -36,5 +39,25 @@ class DatabaseSeeder extends Seeder
             $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
             $group->users()->attach(array_unique([1, ...$users]));
         }
+
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([
+                $message->sender_id,
+                $message->receiver_id,
+            ])->sort()->implode('_');
+        })->map(function ($groupedMessages) {
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon(),
+            ];
+        })->values();
+
+        Conversation::insertOrIgnore($conversations->toArray());
     }
 }

22121d25f76a16638faf6af47611e1f6e28fc931
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-16 20:45:03 +0530

    add state to manage and sort conversation

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -8,9 +8,43 @@ export default function Chat({ children }) {
     const selectedConversation = page.props.selectedConversation;
 
     const [onlineUsers, setOnlineUsers] = useState({});
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
+
+    const isUserOnline = (userId) => onlineUsers[userId];
 
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
+
+    useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                if (a.blocked_at && b.blocked_at) {
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                } else if (a.blocked_at) {
+                    return 1;
+                } else if (b.blocked_at) {
+                    return -1;
+                }
+
+                if (a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.locaeCompare(
+                        a.last_message_date
+                    );
+                } else if (a.last_message_date) {
+                    return -1;
+                } else if (b.last_message_date) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+        );
+    }, [localConversations]);
+
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [conversations]);
 
     useEffect(() => {
         Echo.join("online")

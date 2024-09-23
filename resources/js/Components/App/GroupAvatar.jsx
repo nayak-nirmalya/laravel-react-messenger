512c85bcf4f1b4e47f6321a4b701955bb115e7dc
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-23 20:20:55 +0530

    add content to UserAvatar

---
+++ resources/js/Components/App/GroupAvatar.jsx
@@ -0,0 +1,13 @@
+import { UserIcon } from "@heroicons/react/24/solid";
+
+export default function UserAvatar({}) {
+    return (
+        <div className={`avatar placeholder`}>
+            <div className={`bg-gray-400 text-gray-800 rounded-full w-8`}>
+                <span className="text-xl">
+                    <UserIcon className="w-4" />
+                </span>
+            </div>
+        </div>
+    );
+}

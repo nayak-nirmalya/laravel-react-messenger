cd0946f9ad8bcc9b6126528ab48644d5dea973a7
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-25 20:06:18 +0530

    modified MessageAttachmentResource toArray method

--- app/Http/Resources/MessageAttachmentResource.php
+++ app/Http/Resources/MessageAttachmentResource.php
@@ -4,9 +4,12 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class MessageAttachmentResource extends JsonResource
 {
+    public static $wrap = false;
+
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +17,15 @@ class MessageAttachmentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'message_id' => $this->message_id,
+            'name' => $this->name,
+            'mime' => $this->mime,
+            'size' => $this->size,
+            'url' => Storage::url($this->path),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }

756b24b575d827169f3f68e683981755b90108bc
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-25 20:24:14 +0530

    impl byGroup & loadOlder inside MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,7 +13,7 @@ class MessageController extends Controller
 {
     public function byUser(User $user)
     {
-        $message = Message::where("sender_id", auth()->id())
+        $messages = Message::where('sender_id', auth()->id())
             ->where("receiver_id", $user->id)
             ->orWhere("sender_id", $user->id)
             ->where("receiver_id", auth()->id())
@@ -23,18 +23,46 @@ class MessageController extends Controller
 
         return inertia('Home', [
             'selectedConversation' => $user->toConversationArray(),
-            'messages' => MessageResource::collection($message),
+            'messages' => MessageResource::collection($messages),
         ]);
     }
 
     public function byGroup(Group $group)
     {
+        $messages = Message::where('group_id', $group->id)
+            ->latest()
+            ->paginate(50)
+        ;
 
+        return inertia('Home', [
+            'selectedConversation' => $group->toConversationArray(),
+            'messages' => MessageResource::collection($messages),
+        ]);
     }
 
     public function loadOlder(Message $message)
     {
+        if ($message->group_id) {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where('group_id', $message->group_id)
+                ->latest()
+                ->paginate(10)
+            ;
+        } else {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where(function ($query) use ($message) {
+                    $query->where('sender_id', $message->sender_id)
+                        ->where('receiver_id', $message->receiver_id)
+                        ->orWhere('sender_id', $message->receiver_id)
+                        ->where('receiver_id', $message->sender_id)
+                        ->latest()
+                        ->paginate(10)
+                    ;
+                })
+            ;
+        }
 
+        return MessageResource::collection($messages);
     }
 
     public function store(StoreMessageRequest $request)

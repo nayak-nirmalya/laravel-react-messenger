8f363a076b40d75640d33b77b2718104993de7a8
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-29 21:05:47 +0530

    add helper fn: formatMessageDateLong

---
+++ resources/js/helpers.js
@@ -0,0 +1,26 @@
+export const formatMessageDateLong = (date) => {
+    const now = new Date();
+    const inputDate = new Date(date);
+
+    if (isToday(inputDate)) {
+        return inputDate.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    } else if (isYesterday(inputDate)) {
+        return (
+            "Yesterday " +
+            inputDate.toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+            })
+        );
+    } else if (inputDate.getFullYear() === now.getFullYear()) {
+        return inputDate.toLocaleDateString([], {
+            day: "2-digit",
+            month: "short",
+        });
+    } else {
+        return inputDate.toLocaleDateString();
+    }
+};

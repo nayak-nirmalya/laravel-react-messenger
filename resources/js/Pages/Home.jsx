8c0e670cb501982b74b822d1b61b14c9eb3636a5
Nirmalya Nayak <n.nayak263@gmail.com>
2024-09-29 20:17:16 +0530

    render ChatBubbleLeftRightIcon if no messages present on HomePage

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 import ChatLayout from "@/Layouts/ChatLayout";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { useEffect } from "react";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
 
-function Home({ auth }) {
-    return <>Messages</>;
+function Home({ messages }) {
+    const [localMesssages, setLocalMessages] = useState([]);
+
+    useEffect(() => {
+        setLocalMessages(messages);
+    }, [messages]);
+
+    return (
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                        Please select conversation to see messages
+                    </div>
+                    <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+                </div>
+            )}
+        </>
+    );
 }
 
 Home.layout = (page) => (

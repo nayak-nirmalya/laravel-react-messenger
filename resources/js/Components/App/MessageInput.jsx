cd0604b20a8b5fb3539e5fa72ab68cb0062f729a
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-03 20:26:37 +0530

    add fn: onSendClick

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -8,11 +8,53 @@ import {
 } from "@heroicons/react/24/solid";
 
 import NewMessageInput from "./NewMessageInput";
+import axios from "axios";
 
 export default function MessageInput({ conversation = null }) {
     const [newMessage, setNewMessage] = useState("");
     const [inputErrorMessage, setInputErrorMessage] = useState("");
     const [messageSending, setMessageSending] = useState(false);
+
+    const onSendClick = () => {
+        if (newMessage.trim() === "") {
+            setInputErrorMessage(
+                "Please provide a message or upload attachments."
+            );
+
+            setTimeout(() => {
+                setInputErrorMessage("");
+            }, 3000);
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("message", newMessage);
+
+        if (conversation.is_user) {
+            formData.append("receiver_id", conversation.id);
+        } else if (conversation.is_group) {
+            formData.append("group_id", conversation.id);
+        }
+
+        setMessageSending(true);
+
+        axios
+            .post(route("message.store"), formData, {
+                onUploadProgress: (progressEvent) => {
+                    const progress = Math.round(
+                        (progressEvent.loaded / progressEvent.total) * 100
+                    );
+                    console.log(progress);
+                },
+            })
+            .then((response) => {
+                setNewMessage("");
+                setMessageSending(false);
+            })
+            .catch((error) => {
+                setMessageSending(false);
+            });
+    };
 
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
@@ -41,7 +83,10 @@ export default function MessageInput({ conversation = null }) {
                         value={newMessage}
                         onChange={(ev) => setNewMessage(ev.target.value)}
                     />
-                    <button className="btn btn-info rounded-l-none">
+                    <button
+                        className="btn btn-info rounded-l-none"
+                        onClick={onSendClick}
+                    >
                         {messageSending && (
                             <span className="loading loading-spinner loading-xs"></span>
                         )}

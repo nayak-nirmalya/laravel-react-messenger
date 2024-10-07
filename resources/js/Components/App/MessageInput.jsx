71dc6b7c04a6c0a278f736543ab9c9a5dc41bdd9
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-07 20:25:21 +0530

    add EmojiPicker inside headlessui Popover

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import {
     PaperClipIcon,
     PhotoIcon,
@@ -7,6 +7,8 @@ import {
     PaperAirplaneIcon,
 } from "@heroicons/react/24/solid";
 import axios from "axios";
+import EmojiPicker from "emoji-picker-react";
+import { Popover, PopoverPanel, PopoverButton } from "@headlessui/react";
 
 import NewMessageInput from "./NewMessageInput";
 
@@ -100,9 +102,19 @@ export default function MessageInput({ conversation = null }) {
                 )}
             </div>
             <div className="order-3 xs:order-2 p-2 flex">
-                <button className="p-1 text-gray-400 hover:text-gray-300">
-                    <FaceSmileIcon className="w-6 h-6" />
-                </button>
+                <Popover className="relative">
+                    <PopoverButton className="p-1 text-gray-400 hover:text-gray-300">
+                        <FaceSmileIcon className="w-6 h-6" />
+                    </PopoverButton>
+                    <PopoverPanel className="absolute z-10 right-0 bottom-full">
+                        <EmojiPicker
+                            theme="dark"
+                            onEmojiClick={(ev) =>
+                                setNewMessage(newMessage + ev.emoji)
+                            }
+                        />
+                    </PopoverPanel>
+                </Popover>
                 <button className="p-1 text-gray-400 hover:text-gray-300">
                     <HandThumbUpIcon className="w-6 h-6" />
                 </button>

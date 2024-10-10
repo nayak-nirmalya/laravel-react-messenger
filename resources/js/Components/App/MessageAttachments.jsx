13c9a88dd8c8bbf2b32216d33afb44acd57c9ab7
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-10 20:28:26 +0530

    preview pdf inside MessageAttachments

--- resources/js/Components/App/MessageAttachments.jsx
+++ resources/js/Components/App/MessageAttachments.jsx
@@ -47,6 +47,23 @@ export default function MessageAttachments({ attachments, attachmentClick }) {
                                     <video src={attachment.url}></video>
                                 </div>
                             )}
+                            {isAudio(attachment) && (
+                                <div className="relative flex justify-center items-center">
+                                    <audio
+                                        src={attachment.url}
+                                        controls
+                                    ></audio>
+                                </div>
+                            )}
+                            {isPDF(attachment) && (
+                                <div className="absolute flex justify-center items-center">
+                                    <div className="absolute left-0 top-0 right-0 bottom-0"></div>
+                                    <iframe
+                                        src={attachment.url}
+                                        className="w-full h-full"
+                                    ></iframe>
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>

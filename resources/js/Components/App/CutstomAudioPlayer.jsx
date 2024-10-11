9a5f4269c3a46a06304f06189583d648ad00e878
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-11 19:46:54 +0530

    add audio element and input control to CutstomAudioPlayer component

--- resources/js/Components/App/CutstomAudioPlayer.jsx
+++ resources/js/Components/App/CutstomAudioPlayer.jsx
@@ -42,5 +42,42 @@ export default function CutstomAudioPlayer({ file, showVolume = true }) {
     const handleSeekChange = (ev) => {
         const time = ev.target.value;
         audioRef.current.currentTime = time;
+        setCurrentTime(time);
     };
+
+    return (
+        <div className="w-full flex items-center gap-2 py-2 px-3 rounded-md bg-slate-800">
+            <audio
+                ref={audioRef}
+                src={file.url}
+                controls
+                onTimeUpdate={handleTimeUpdate}
+                onLoadedMetadata={handleLoadedMetadata}
+                className="hidden"
+            />
+            <button onClick={togglePlayPause}>
+                {isPlaying && <PauseCircleIcon className="w-6 text-gray-400" />}
+                {!isPlaying && <PlayCircleIcon className="w-6 text-gray-400" />}
+            </button>
+            {showVolume && (
+                <input
+                    type="range"
+                    min="0"
+                    max="1"
+                    step="0.01"
+                    value={volume}
+                    onChange={handleVolumeChange}
+                />
+            )}
+            <input
+                type="range"
+                className="flex-1"
+                min="0"
+                max={duration}
+                step="0.01"
+                value={currentTime}
+                onChange={handleSeekChange}
+            />
+        </div>
+    );
 }

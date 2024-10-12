02b9f074f7e7bbda12bd50b4ff8537c7dc021070
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-12 14:27:34 +0530

    add memoized values: attachment & previewableAttachments

--- resources/js/Components/App/AttachmentPreviewModal.jsx
+++ resources/js/Components/App/AttachmentPreviewModal.jsx
@@ -14,4 +14,14 @@ export default function AttachmentPreviewModal({
     index,
     show = false,
     onClose = () => {},
-}) {}
+}) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const attachment = useMemo(() => {
+        return attachments[currentIndex];
+    }, [attachments, currentIndex]);
+
+    const previewableAttachments = useMemo(() => {
+        return attachments.filter((attachment) => isPreviewable(attachment));
+    }, [attachments]);
+}

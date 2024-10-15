edfeb7a948c906254dfeb481ecb019cba1094bfe
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-15 09:48:55 +0530

    add fn: recordedAudioReady

--- resources/js/Components/App/AudioRecorder.jsx
+++ resources/js/Components/App/AudioRecorder.jsx
@@ -1,11 +1,17 @@
 import { MicrophoneIcon, StopCircleIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
-export default function AudioRecorder() {
+export default function AudioRecorder({ fileReady }) {
     const [recording, setRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+
+    const onMicrophoneClick = async () => {};
 
     return (
-        <button>
+        <button
+            onClick={onMicrophoneClick}
+            className="p-1 text-gray-400 hover:text-gray-200"
+        >
             {recording && <StopCircleIcon className="w-6 text-red-600" />}
             {!recording && <MicrophoneIcon className="w-6" />}
         </button>
--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -16,6 +16,7 @@ import { isAudio, isImage } from "@/helpers";
 import NewMessageInput from "./NewMessageInput";
 import AttachmentPreview from "./AttachmentPreview";
 import CutstomAudioPlayer from "./CutstomAudioPlayer";
+import AudioRecorder from "./AudioRecorder";
 
 export default function MessageInput({ conversation = null }) {
     const [newMessage, setNewMessage] = useState("");
@@ -109,6 +110,12 @@ export default function MessageInput({ conversation = null }) {
         axios.post(route("message.store"), data);
     };
 
+    const recordedAudioReady = (file, url) => {
+        setChosenFiles((prevFiles) => {
+            return [...prevFiles, { file, url }];
+        });
+    };
+
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
@@ -131,6 +138,7 @@ export default function MessageInput({ conversation = null }) {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
+                <AudioRecorder fileReady={recordedAudioReady} />
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">

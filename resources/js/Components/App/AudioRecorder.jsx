90186def34a24276305ed056bae566890c9c40a3
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-15 10:01:12 +0530

    add fn: onMicrophoneClick to get user permission for audio and record

--- resources/js/Components/App/AudioRecorder.jsx
+++ resources/js/Components/App/AudioRecorder.jsx
@@ -5,7 +5,52 @@ export default function AudioRecorder({ fileReady }) {
     const [recording, setRecording] = useState(false);
     const [mediaRecorder, setMediaRecorder] = useState(null);
 
-    const onMicrophoneClick = async () => {};
+    const onMicrophoneClick = async () => {
+        if (recording) {
+            setRecording(false);
+
+            if (mediaRecorder) {
+                mediaRecorder.stop();
+                setMediaRecorder(null);
+            }
+
+            return;
+        }
+
+        setRecording(true);
+
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            });
+            const newMediaRecorder = new MediaRecorder(stream);
+            const chunks = [];
+
+            newMediaRecorder.addEventListener("dataavailable", (event) => {
+                chunks.push(event.data);
+            });
+
+            newMediaRecorder.addEventListener("stop", () => {
+                let audioBlob = new Blob(chunks, {
+                    type: "audio/ogg; codec=opus",
+                });
+
+                let audioFile = new File([audioBlob], "recorded_audio.ogg", {
+                    type: "audio/ogg; codec=opus",
+                });
+
+                const url = URL.createObjectURL(audioFile);
+
+                fileReady(audioFile, url);
+            });
+
+            newMediaRecorder.start();
+            setMediaRecorder(newMediaRecorder);
+        } catch (error) {
+            setRecording(false);
+            console.error("[ERROR_ACCESSING_MICROPHONE]", error);
+        }
+    };
 
     return (
         <button

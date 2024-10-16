be0bb0480a471dca2c4c04379501b31a41d8ebed
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-16 09:37:39 +0530

    show toasts & auto-remove them after 5000ms

--- resources/js/Components/App/Toast.jsx
+++ resources/js/Components/App/Toast.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import { useEventBus } from "@/EventBus";
 
@@ -7,14 +8,28 @@ export default function Toast({}) {
 
     const { on } = useEventBus();
 
+    useEffect(() => {
+        on("toast.show", (message) => {
+            const uuid = uuidv4();
+
+            setToasts((oldToasts) => [...oldToasts, { message, uuid }]);
+
+            setTimeout(() => {
+                setToasts((oldToasts) =>
+                    oldToasts.filter((toast) => toast.uuid !== uuid)
+                );
+            }, 5000);
+        });
+    }, [on]);
+
     return (
         <div className="toast min-w-[240px]">
             {toasts.map((toast, index) => (
                 <div
-                    key={index}
+                    key={toast.uuid}
                     className="alert alert-success py-3 px-4 text-gray-100 rounded-md"
                 >
-                    <span>New message arrived.</span>
+                    <span>{toast.message}</span>
                 </div>
             ))}
         </div>

2b99d37e9b1cdea3fb2c9faf0d791c3c858bbc7f
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-20 10:21:01 +0530

    modify destroy fn inside MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -130,9 +130,13 @@ class MessageController extends Controller
         $message->delete();
 
         if ($group) {
+            $group = Group::find($group->id);
             $lastMessage = $group->lastMessage;
+        } else if ($conversation) {
+            $conversation = Conversation::find($conversation->id);
+            $lastMessage = $conversation->lastMessage;
         }
 
-        return response()->json(['message' => new MessageResource($lastMessage)]);
+        return response()->json(['message' => $lastMessage ? new MessageResource($lastMessage) : null]);
     }
 }

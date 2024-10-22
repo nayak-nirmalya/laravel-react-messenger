e9adf1f0c3ea37293c430e38ee7273a4375ae45b
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-22 12:33:40 +0530

    add multiple form fields inside GroupModal

--- resources/js/Components/App/GroupModal.jsx
+++ resources/js/Components/App/GroupModal.jsx
@@ -68,5 +68,70 @@ export default function GroupModal({ show = false, onClose = () => {} }) {
         });
     }, [on]);
 
-    return <></>;
+    return (
+        <Modal show={show} onClose={closeModal}>
+            <form
+                onSubmit={createOrUpdateGroup}
+                className="p-6 overflow-y-auto"
+            >
+                <h2 className="text-xl font-medium text-gray-900 dark:text-gray-100">
+                    {group.id
+                        ? `Edit Group "${group.name}`
+                        : "Create new Group"}
+                </h2>
+                <div className="mt-8">
+                    <InputLabel htmlFor="name" value="Name" />
+                    <TextInput
+                        id="name"
+                        className="mt-1 block w-full"
+                        value={data.name}
+                        disabled={!!group.id}
+                        onChange={(e) => setData("name", e.target.value)}
+                        required
+                        isFocused
+                    />
+                    <InputError className="mt-2" message={errors.name} />
+                </div>
+                <div className="mt-4">
+                    <InputLabel htmlFor="description" value="Description" />
+                    <TextAreaInput
+                        id="description"
+                        rows="3"
+                        className="mt-1 block w-full"
+                        value={data.description || ""}
+                        onChange={(e) => setData("description", e.target.value)}
+                    />
+                    <InputError className="mt-2" message={errors.description} />
+                </div>
+                <div className="mt-4">
+                    <InputLabel value="Select Users" />
+                    <UserPicker
+                        value={
+                            users.filter(
+                                (u) =>
+                                    group.owner_id !== u.id &&
+                                    data.user_ids.includes(u.id)
+                            ) || []
+                        }
+                        options={users}
+                        onSelect={(users) =>
+                            setData(
+                                "user_ids",
+                                users.map((u) => u.id)
+                            )
+                        }
+                    />
+                    <InputError className="mt-2" message={errors.user_ids} />
+                </div>
+                <div className="mt-6 flex justify-end">
+                    <SecondaryButton onClick={closeModal}>
+                        Cancel
+                    </SecondaryButton>
+                    <PrimaryButton className="ms-3" disabled={processing}>
+                        {group.id ? "Update" : "Create"}
+                    </PrimaryButton>
+                </div>
+            </form>
+        </Modal>
+    );
 }

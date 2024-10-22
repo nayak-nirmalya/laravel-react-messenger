6fd6b2d1dc6efcedffb17dcdace9014a826b5328
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-22 11:58:51 +0530

    add createOrUpdateGroup inside GroupModal.jsx

--- resources/js/Components/App/GroupModal.jsx
+++ resources/js/Components/App/GroupModal.jsx
@@ -12,4 +12,43 @@ import UserPicker from "@/Components/App/UserPicker";
 
 import { useEventBus } from "@/EventBus";
 
-export default function GroupModal({ show = false, onClose = () => {} }) {}
+export default function GroupModal({ show = false, onClose = () => {} }) {
+    const page = usePage();
+    const conversations = page.props.conversations;
+
+    const { on, emit } = useEventBus();
+
+    const [group, setGroup] = useState({});
+
+    const { data, setData, processing, reset, post, put, errors } = useForm({
+        id: "",
+        name: "",
+        description: "",
+        user_ids: [],
+    });
+
+    const users = conversations.filter((c) => !c.is_group);
+
+    const createOrUpdateGroup = (e) => {
+        e.preventDefault();
+
+        if (group.id) {
+            put(route("group.update", group.id), {
+                onSuccess: () => {
+                    closeModal();
+                    emit("toast.show", `Group "${data.name}" was updated.`);
+                },
+            });
+            return;
+        }
+
+        post(route("group.store"), {
+            onSuccess: () => {
+                emit("toast.show", `Group "${data.name}" was created.`);
+                closeModal();
+            },
+        });
+    };
+
+    return <></>;
+}

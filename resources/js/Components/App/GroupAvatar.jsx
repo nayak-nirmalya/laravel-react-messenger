4d15488e81b1435b4cc836a5e26ac9d8dd9a017b
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-23 12:56:16 +0530

    ui bug fix: show UsersIcon instead of UserIcon inside GroupAvatar

--- resources/js/Components/App/GroupAvatar.jsx
+++ resources/js/Components/App/GroupAvatar.jsx
@@ -1,11 +1,11 @@
-import { UserIcon } from "@heroicons/react/24/solid";
+import { UsersIcon } from "@heroicons/react/24/solid";
 
 export default function GroupAvatar({}) {
     return (
         <div className={`avatar placeholder`}>
             <div className={`bg-gray-400 text-gray-800 rounded-full w-8`}>
                 <span className="text-xl">
-                    <UserIcon className="w-4" />
+                    <UsersIcon className="w-4" />
                 </span>
             </div>
         </div>

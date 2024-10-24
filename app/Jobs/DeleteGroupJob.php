85a926e2bb63a704f0009d4a654df3ba5f23f278
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-24 11:05:07 +0530

    create new event: GroupDeleted that implements ShouldBroadcastNow

--- app/Jobs/DeleteGroupJob.php
+++ app/Jobs/DeleteGroupJob.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Events\GroupDeleted;
+use App\Models\Group;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
@@ -15,7 +17,7 @@ class DeleteGroupJob implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(public Group $group)
     {
         //
     }
@@ -25,6 +27,18 @@ class DeleteGroupJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        GroupDeleted::dispatch($this->group);
+
+        $this->group->last_message_id = null;
+        $this->group->save();
+
+        // iterate over all messages and delete them including media files
+        $this->group->messages->each->delete();
+
+        // remove all users from the group
+        $this->group->users()->detach();
+
+        // finally delete the group itself
+        $this->group->delete();
     }
 }

ba5ef2cdb0d3a44c4825781e433fdfa8b43dfb33
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-29 12:35:43 +0530

    define store method to save User to DB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'is_admin' => 'boolean',
+        ]);
 
+        $rawPassword = Str::random(8);
+        $data['password'] = bcrypt($rawPassword);
+        $data['email_verified_at'] = now();
+
+        $user = User::create($data);
+
+        return redirect()->back();
     }
 
     public function changeRole(User $user)

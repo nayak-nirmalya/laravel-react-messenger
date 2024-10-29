100047f0a09891db5fcd07809b1a1a006f79fad6
Nirmalya Nayak <n.nayak263@gmail.com>
2024-10-29 12:42:25 +0530

    define blockUnblock & changeRole function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,11 +27,24 @@ class UserController extends Controller
 
     public function changeRole(User $user)
     {
+        $user->update(['is_admin' => !(bool) $user->is_admin]);
 
+        $message = "User role was changed into " . ($user->is_admin ? '"Admin"' : '"Regular User"');
+
+        return response()->json(['message' => $message]);
     }
 
     public function blockUnblock(User $user)
     {
+        if ($user->blocked_at) {
+            $user->blocked_at = null;
+            $message = "Your Account has been activated.";
+        } else {
+            $user->blocked_at = now();
+            $message = "Your Account has been blocked.";
+        }
+        $user->save();
 
+        return response()->json(['message' => $message]);
     }
 }

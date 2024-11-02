41d397fb21b2f818186d0b18c8fc1fc05816f974
Nirmalya Nayak <n.nayak263@gmail.com>
2024-11-02 09:44:10 +0530

    add markdown to created.blade.php

---
+++ resources/views/mail/user/created.blade.php
@@ -0,0 +1,18 @@
+<x-mail::message>
+  Hello {{ $user->name }},
+
+  Your account has been created successfully.
+
+  **Here is your login information:** <br>
+  E-Mail: {{ $user->email }} <br>
+  Password: {{ $password }} <br>
+
+  Please login to the system and change your password.
+
+  <x-mail::button url="{{ route('login') }}">
+    Click here to login
+  </x-mail::button>
+
+  Thank you, <br>
+  {{ config('app.name') }}
+</x-mail::message>
